ad7432465e4f0bd7695f9eb6f5854e373dc75d36
SRIRAM P <165500350+Sriramofficialsit@users.noreply.github.com>
2024-04-30 22:04:31 +0530

    Display All Contries Flags

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-let obj1 = { name: "Person 1", age: 5 };
-let obj2 = { age: 5, name: "Person 1" };
-let obj1String = JSON.stringify(obj1, Object.keys(obj1));
-let obj2String = JSON.stringify(obj2, Object.keys(obj2));
-let areEqual = obj1String === obj2String;
-console.log("Are the objects equal?", areEqual);
+var request = new XMLHttpRequest();
+request.open("GET","https://restcountries.com/v3.1/all");
+request.send();
+request.onload = function(){
+    var res = JSON.parse(request.response);
+    console.log(res);
+    for(var i=0;i<res.length;i++)
+    {
+        console.log(res[i].flags.svg)
+    }
+}
